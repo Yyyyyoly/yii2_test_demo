827c2d153587ab3d6a368ad7099d74d16380b8dd
wangyaoguang <wangyaoguang@sunlands.com>
2018-04-10 17:42:23 +0800

    本地部署了jre8/jdk8+tomcat+cas-server 4.0的cas服务器端 修改了系统的入口 把登录验证拉入了底层（yii2没有中间件不知道怎么处理好一点。。） 同时发现了phpcas的一个客户端底层client对于https转http没有做处理

--- config/params.php
+++ config/params.php
@@ -3,8 +3,8 @@
 return [
     'adminEmail' => 'admin@example.com',
     'cas'   => [
-        'cas_host' => '172.16.116.136', //CAS Server 主机
-        'cas_port' => 9091, // CAS server端口
+        'cas_host' => 'localhost', //CAS Server 主机
+        'cas_port' => 8888, // CAS server端口
         'cas_context' => '/cas', // CAS Server 路径
         'cas_server_ca_cert_path' => './ssoserver.cer' // <span lang="EN-US">CAS Server </span>证书
     ],
--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -7,42 +7,14 @@ require_once __DIR__ . '/../vendor/jasig/phpcas/CAS.php';
 
 
 use Yii;
-use yii\filters\AccessControl;
-use yii\web\Controller;
 use yii\web\Response;
-use yii\filters\VerbFilter;
+use yii\base\Controller;
 use app\models\ContactForm;
 use phpCAS;
 
-class SiteController extends Controller
+class SiteController extends BaseController
 {
     public $layout = 'main';
-
-    /**
-     * {@inheritdoc}
-     */
-    public function behaviors()
-    {
-        return [
-            'access' => [
-                'class' => AccessControl::className(),
-                'only' => ['logout'],
-                'rules' => [
-                    [
-                        'actions' => ['logout'],
-                        'allow' => true,
-                        'roles' => ['@'],
-                    ],
-                ],
-            ],
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'logout' => ['post'],
-                ],
-            ],
-        ];
-    }
 
     /**
      * {@inheritdoc}
@@ -70,54 +42,6 @@ class SiteController extends Controller
         return $this->render('home', ['name' => '首页', 'message' => '这里是测试的首页']);
     }
 
-    /**
-     * Login action.
-     *
-     * @return Response|string
-     */
-    public function actionLogin()
-    {
-        // Initialize phpCAS
-        phpCAS::client(
-            CAS_VERSION_2_0,
-            Yii::$app->params['cas']['cas_host'],
-            Yii::$app->params['cas']['cas_port'],
-            Yii::$app->params['cas']['cas_context'],
-            true
-        );
-
-        //登陆成功后跳转的地址 -- 登陆方法中加此句
-        $cas_server_url = Yii::$app->params['cas']['cas_host'].':'.Yii::$app->params['cas']['cas_port'].Yii::$app->params['cas']['cas_context'].'/login?embed=true';
-        $own_server_url = Yii::$app->request->getHostInfo().Yii::$app->request->getPathInfo();
-        phpCAS::setServerLoginUrl('http://'.$cas_server_url.'&'.$own_server_url);
-
-        // For production use set the CA certificate that is the issuer of the cert
-        // on the CAS server and uncomment the line below
-        // phpCAS::setCasServerCACert($cas_server_ca_cert_path);
-
-        // For quick testing you can disable SSL validation of the CAS server.
-        // THIS SETTING IS NOT RECOMMENDED FOR PRODUCTION.
-        // VALIDATING THE CAS SERVER IS CRUCIAL TO THE SECURITY OF THE CAS PROTOCOL!
-        phpCAS::setNoCasServerValidation();
-
-        //这里会检测服务器端的退出的通知，就能实现php和其他语言平台间同步登出了
-        phpCAS::handleLogoutRequests();
-
-        // force CAS authentication
-        if (phpCAS::checkAuthentication()) {
-            //获取登陆的用户名
-            $username = phpCAS::getUser();
-            $cas_user_id = phpCAS::getAttribute('id');
-            Yii::info($cas_user_id.'_'.$username, __METHOD__);
-            //用户登陆成功后
-            return $this->goHome();
-        } else {
-            // 访问CAS的验证
-            phpCAS::forceAuthentication();
-            exit;
-        }
-    }
-
 
     /**
      * Logout action.
@@ -134,7 +58,7 @@ class SiteController extends Controller
 
         $cas_server_url = Yii::$app->params['cas']['cas_host'].':'.Yii::$app->params['cas']['cas_port'].Yii::$app->params['cas']['cas_context'].'/logout?embed=true';
         $own_home_url = Yii::$app->getHomeUrl();
-        phpCAS::setServerLoginUrl('http://'.$cas_server_url.'&'.$own_home_url);
+        phpCAS::setServerLogoutUrl('http://'.$cas_server_url.'&service='.$own_home_url);
         //no SSL validation for the CAS server
         phpCAS::setNoCasServerValidation();
         phpCAS::logout();
